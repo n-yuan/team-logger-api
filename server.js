e4a41fe2c23a38b3debc4c1eb1a9b82b51fdddd2
Nan Yuan <nan.yuan.dev@gmail.com>
2020-09-17 23:12:55 +1000

    Connect MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+//Connect Database
+connectDB();
 
 //Define Routes
 app.use("/api/logs", require("./routes/logs"));
